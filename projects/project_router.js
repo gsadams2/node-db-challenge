36b3ee3fd517721671ec16b25c4f6957532a3e25
George Adams <gadams@Georges-Air.local>
2019-07-19 12:49:15 -0500

    minor changes

--- projects/project_router.js
+++ projects/project_router.js
@@ -62,4 +62,22 @@ router.post("/:id/actions", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Projects.findById(id);
+
+    if (project) {
+      res.json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find project with given id." });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get project" });
+  }
+});
+
 module.exports = router;
